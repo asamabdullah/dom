a3cb137405e5b5f97d97582a2935e4fe205da2a4
asamabdullah <149607707+asamabdullah@users.noreply.github.com>
2024-04-07 15:54:20 +0600

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,574 @@
+/*
+var friendsName = "abu";
+console.log(friendsName)
+*/
+
+
+
+
+
+
+
+
+
+/*
+var strtype = "NFDNRGOJNVOI";
+console.log(strtype)
+*/
+
+
+
+
+
+
+
+
+
+/*
+var numtype = 55652512525;
+console.log(numtype)
+*/
+
+
+
+
+
+
+
+
+
+/*
+var booltype = false ;
+console.log(typeof booltype)
+*/
+
+
+
+
+
+
+
+/*
+var rochona = "hello world";
+console.log(rochona.toUpperCase())
+*/
+
+
+
+
+
+
+
+/*
+var num1 = 35;
+var num2 = 65;
+
+var total = num1 + num2;
+console.log(total)
+*/
+
+
+
+
+
+
+
+
+
+/*
+var fame = " ab ah far rah tau";
+console.log(fame.indexOf("tau"))
+*/
+
+
+
+
+
+
+
+
+/*
+var num = 68;
+var fum = 72;
+
+num = "" + fum;
+console.log(num+fum);
+*/
+
+
+
+
+
+
+/*
+var num1 = 35;
+var num2 = 65;
+
+var total = num1 - num2;
+var sum = Math.abs(total)
+console.log(sum)
+*/
+
+
+
+
+
+
+
+/*
+var totalMembers = 10;
+var voter = 4.9999999
+
+var sum = Math.round(voter)
+console.log(sum)
+*/
+
+
+
+
+
+
+
+/*
+var totalMembers = 10;
+var voter = 4.9999999
+
+var sum = Math.ceil(voter)
+console.log(sum)
+*/
+
+
+
+
+
+
+
+
+/*
+var totalMembers = 10;
+var voter = 4.9999999
+
+var sum = Math.floor(voter)
+console.log(sum)
+*/
+
+
+
+
+
+
+
+/*
+var lotary = Math.random()*100
+console.log(lotary)
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+var a = 2;
+var b = 5;
+
+
+if(a>b){
+    console.log("not right")}
+
+if(a<b){
+    console.log("right")}
+*/
+
+
+
+
+
+
+
+
+
+/*
+var a = 9;
+var b = 9;
+
+
+if(a<b){
+   console.log("b boro")}
+
+
+else if(a>b){
+    console.log("a boro")}    
+
+
+else{
+    console.log("same")}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var sum = 89;
+
+
+switch(sum){
+    case 10:
+        console.log("it's right now");
+        break;
+   case 56:
+        consoloe.log("i think that could be right there now");
+    break;
+    case 89:
+        console.log("oh that sounds pretty good");
+        break;
+    default:
+        console.log("i didn't found anything");
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var myDate = new Date();
+
+console.log(myDate.getFullYear())
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getMonth())
+console.log(myDate.getDate())
+console.log(myDate.getHours())
+console.log(myDate.getMinutes())
+console.log(myDate.getSeconds())
+console.log(myDate.getMilliseconds())
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var dum = [ "ab" , "ah" , "far" , "rah" , "tau"];
+dum[2] = 96;
+
+console.log(dum);
+*/
+
+
+
+
+
+
+
+/*
+var dum = [ "ab" , "ah" , "far" , "rah" , "tau"];
+dum.push("jfjdinfmn")
+
+console.log(dum);
+*/
+
+
+
+
+
+
+
+/*
+var dum = [ "ab" , "ah" , "far" , "rah" , "tau"];
+dum.pop();
+
+console.log(dum);
+*/
+
+
+
+
+
+
+
+
+/*
+var dum = [ "ab" , "ah" , "far" , "rah" , "tau"];
+dum.unshift("hfhhd");
+dum.shift()
+
+console.log(dum);
+*/
+
+
+
+
+
+
+
+
+/*
+var dum = [ "ab" , "ah" , "far" , "rah" , "tau"];
+dum.splice(1,1);
+dum.splice(3,4);
+
+console.log(dum);
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+for(var sum = 1; sum   ){
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var  fum = "ur sonar bangla 1971";
+
+switch(fum){
+    case "ur sonar bangla 1971":
+    console.log(fum.push("amr sonar banga"));
+    break;
+    case "ur sonar bangla":
+    console.log(fum.push("ur sonar banga"));
+    break;
+    default:
+        console.log("bcbcbcbcbcbcbcbcbcb")
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+function Lam(){
+    var num = 45;
+    var mun = 14;
+    console.log(num-mun);
+}
+
+Lam()
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function MyInfo(father,mother,home){
+    this.father = father;
+    this.mother = mother;
+    this.home = home;
+}
+
+var fum = new MyInfo("Azad","Sumaiya","Block:H")
+console.log(fum)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var tf = 'foknfik';
+
+switch (tf){
+    case 'ijfgif':
+    console.log('fault is given');
+        break;
+    case 'kfoknfik':
+    console.log('lault given');
+        break;
+    default:
+        console.log('error found');
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var fum = 45;
+
+var condition = (fum > 95) ? 'i am big' : 'you are big';
+
+console.log(condition);
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+for(var i = 4; i <= 25; i++  ){
+    
+
+    if( i == 8){
+        console.log("it's ok");
+        break;
+    }
+
+    console.log(i);
+}
+*/
+
+
+
+
+
+
+
+
+
+/*
+function no(){
+
+var hjd = 'gfide';
+
+    console.log(hjd)
+}
+
+no()
+*/
+
+
+
+
+
+
+
+
+
+/*
+{
+let ita = 'i am good'
+
+console.log(ita);
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var num = function(message, message2){
+    console.log(message , message2)
+}
+num('i like js' , ',i like Pyhton')
+*/
+
+
+
+
+
+
+
+
+
+/*
+var world = 'eshan';
+let bd = 'hablu eshan';
+
+function World(abs){
+    function bd(){
+    console.log(abs);
+    }
+    bd()
+    console.log('abs')
+}
+
+
+World('')
+*/
